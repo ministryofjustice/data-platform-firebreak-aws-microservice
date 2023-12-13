1e4c43ac526cc0d0702d91f1f75d1158ab1cfff5
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2023-12-13 17:57:14 +0000

    Add boto3 and patch test with moto

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,31 +1,16 @@
-from datetime import datetime
-
+import boto3
 import pytest
+from moto import mock_iam
 
 
 @pytest.fixture
-def get_role_response():
-    return {
-        "Roles": [
-            {
-                "Path": "string",
-                "RoleName": "string",
-                "RoleId": "string",
-                "Arn": "string",
-                "CreateDate": datetime(2015, 1, 1),
-                "AssumeRolePolicyDocument": "string",
-                "Description": "string",
-                "MaxSessionDuration": 123,
-                "PermissionsBoundary": {
-                    "PermissionsBoundaryType": "PermissionsBoundaryPolicy",
-                    "PermissionsBoundaryArn": "string",
-                },
-                "Tags": [
-                    {"Key": "string", "Value": "string"},
-                ],
-                "RoleLastUsed": {"LastUsedDate": datetime(2015, 1, 1), "Region": "string"},
-            },
-        ],
-        "IsTruncated": True | False,
-        "Marker": "string",
-    }
+def iam_client(autouse=True):
+    with mock_iam():
+        iam = boto3.client("iam")
+        yield iam
+
+
+@pytest.fixture
+def iam_role(iam_client):
+    iam = boto3.client("iam")
+    return iam.create_role(RoleName="example-role", AssumeRolePolicyDocument="example")
