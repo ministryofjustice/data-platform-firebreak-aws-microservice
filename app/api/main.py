a004f95a976c6f33053d3a9b83b0839c2eff6d6c
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2023-12-21 08:59:11 +0000

    Remove unused code in main

--- app/api/main.py
+++ app/api/main.py
@@ -1,17 +1,9 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from fastapi.security import HTTPBearer
 
 from app.api.routers import role
-from app.core import databases, models
 
 load_dotenv()
-
-# TODO remove if DB not used
-models.Base.metadata.create_all(bind=databases.engine)
-
-# require authentication for all endpoints
-token_auth_scheme = HTTPBearer()
 
 # initialise the app with routes
 app = FastAPI()
